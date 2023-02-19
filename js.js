8e89abfe1119dbc0bca12e70700d03ed283e7f87
Janka172 <jankahegedus872@gmail.com>
2023-02-19 21:10:16 +0100

    mmásképp de legalább fut

--- js.js
+++ js.js
@@ -128,34 +128,3 @@ if (Caption=="C")  reset(0);
 if(document.calculator.LED.value[0] == ".")
    document.calculator.LED.value = "0" + document.calculator.LED.value;
 }
-
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-function alt(){
-  var ki='<h3><b>Általános számológép</b></h3><strong><table class="tdszamologep" border="2" align="center" cellpadding="1" cellspacing="5"><tr> <td colspan="4" class="eredmeny"> <input name="LED" type="text" value="0" > </td><td><input name="=" type="button" id="egyenlo" value="  =  " onclick="Click("=")" ></td></tr><tr> <td><input name="7" type="button" id="het" value="  7  " onclick="Click("7")"> </td><td><input name="8" type="button" id="nyolc" value="  8  " onclick="Click("8")"> </td><td><input name="9" type="button" id="kilenc" value="  9  " onclick="Click("9")"> </td><td><input name="+" type="button" id="adas" value="  +  " onclick="Click("+")"> </td><td><input name="^" type="button" id="findex" value="  ^  " onclick="Click("^")"> </td></tr><tr> <td><input name="4" type="button" id="negy" value="  4  " onclick="Click("4")"> </td><td><input name="5" type="button" id="otos" value="  5  " onclick="Click("5")"> </td><td><input name="6" type="button" id="hat" value="  6  " onclick="Click("6")"> </td><td><input name="-" type="button" id="vonas"  value="  -   " onclick="Click("-")"> </td><td><input name="1/x" type="button" id="egypx" value=" 1/x" onclick="Click("1/x")"></td></tr><tr> <td><input name="1" type="button" id="egy" value="  1  " onclick="Click("1")"> </td><td><input name="2" type="button" id="ketto" value="  2  " onclick="Click("2")"> </td><td><input name="3" type="button" id="harom" value="  3  " onclick="Click("3")"> </td><td><input name="*" type="button" id="szorzas" value="  *   " onclick="Click("*")"> </td><td><input name="pi" type="button" id="pitag" value="  pi " onclick="Click("pi")"></td></tr><tr> <td><input type="button" name="C" id="torol" value="  C  " onclick="Click("C")"></td><td><input name="0" type="button" id="nulla" value="  0  " onclick="Click("0")"> </td><td><input name="." type="button" id="tized" value="   .  " onclick="Click(".")"> </td><td><input name="+/-" type="button" id="pvmin" value=" +/- " onclick="Click("+/-")"> </td><td><input name="/" type="button" id="osztas" value="  /   " onclick="Click("/")"> </td></tr></table></strong>'
-  document.calculator.innerHTML=ki;
-}
-
-function tuf(){
-  var ki='<h3><b>Tudományos számológép</b></h3><form name="calculator2"><strong><table class="tdszamologep" border="2" align="center" cellpadding="1" cellspacing="5"><tr> <td colspan="4" class="eredmeny"> <input name="LED" type="text" value="0" > </td><td><input type="button" name="C" id="torol" value="  C  " onclick="Click('C')" ></td><td><input name="=" type="button" id="egyenlo" value="  =  " onclick="Click('=')" ></td></tr><tr> <td colspan="2" class="funkcio"> <input name="angle" type="radio" checked> Degrees  </td><td colspan="2" class="funkcio"> <input name="angle" type="radio"> Radians   </td><td colspan="2" class="funkcio"> <input name="angle" type="radio"> Gradients </td></tr><tr> <td> <input name="sin" type="button" id="sin" value=" sin " onclick="Click('sin')" ></td><td> <input name="cos" type="button" id="cos" value=" cos" onclick="Click('cos')" ></td><td> <input name="tab" type="button" id="tan" value=" tan " onclick="Click('tan')" ></td><td> <input name="sinh" type="button" id="sinh" value="sinh" onclick="Click('sinh')" ></td><td> <input name="cosh" type="button" id="cosh" value="cosh" onclick="Click('cosh')" ></td><td> <input name="tanh" type="button" id="tanh" value="tanh" onclick="Click('tanh')" ></td></tr><tr> <td> <input name="asin" type="button" id="asin" value=" asin "onclick="Click('asin')" >
-  </td><td> <input name="acos" type="button" id="acos" value=" acos "onclick="Click('acos')" ></td><td> <input name="atan" type="button" id="atan" value=" atan "onclick="Click('atan')" ></td><td> <input name="asinh" type="button" id="asinh" value="asinh " onclick="Click('asinh')" ></td><td> <input name="acosh" type="button"id="acosh"value="acosh" onclick="Click('acosh')" ></td><td> <input name="atanh" type="button" id="atanh" value="atanh" onclick="Click('atanh')" ></td></tr><tr> <td><input name="exp" type="button" id="exp" value="exp" onclick="Click('exp')" ></td><td><input name="7" type="button" id="het" value="  7  " onclick="Click('7')"> </td><td><input name="8" type="button" id="nyolc" value="  8  " onclick="Click('8')"> </td><td><input name="9" type="button" id="kilenc" value="  9  " onclick="Click('9')"> </td><td><input name="+" type="button" id="adas" value="  +  " onclick="Click('+')" > </td><td><input name="^" type="button" id="findex" value="  ^  " onclick="Click('^')" > </td></tr><tr> <td><input name="log" type="button" id="log" value="log " onclick="Click('log')" ></td><td><input name="4" type="button" id="negy" value="  4  " onclick="Click('4')"> </td><td><input name="5" type="button" id="otos" value="  5  " onclick="Click('5')"> </td><td><input name="6" type="button" id="hat" value="  6  " onclick="Click('6')"> </td><td><input name="-" type="button" id="vonas"  value="  -   " onclick="Click('-')" > </td><td><input name="1/x" type="button" id="egypx" value=" 1/x" onclick="Click('1/x')" ></td></tr><tr>
-<td><input name="ln" type="button" id="abs22" value=" ln  " onclick="Click('ln')" ></td>
-<td><input name="1" type="button" id="egy" value="  1  " onclick="Click('1')"> </td>
-<td><input name="2" type="button" id="ketto" value="  2  " onclick="Click('2')"> </td>
-<td><input name="3" type="button" id="harom" value="  3  " onclick="Click('3')"> </td>
-<td><input name="*" type="button" id="szorzas" value="  *   " onclick="Click('*')" > </td>
-<td><input name="pi" type="button" id="pitag" value="  pi " onclick="Click('pi')" ></td>
-</tr>
-
-<tr> 
-<td><input name="sqrt" type="button" id="sqrt" value="sqrt" onclick="Click('sqrt')" ></td>
-<td><input name="0" type="button" id="nulla" value="  0  " onclick="Click('0')"> </td>
-<td><input name="." type="button" id="tized" value="   .  " onclick="Click('.')" > </td>
-<td><input name="+/-" type="button" id="pvmin" value=" +/- " onclick="Click('+/-')" > </td>
-<td><input name="/" type="button" id="osztas" value="  /   " onclick="Click('/')" > </td>
-<td><input name="e" type="button" id="etag" value="  e  " onclick="Click('e')" > </td>
-</tr>
-
-</table>
-</strong>'
-}
