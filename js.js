6b758ec80661a709bd14fce2ad7d447333ef1769
Janka172 <jankahegedus872@gmail.com>
2023-02-22 17:35:04 +0100

    fa fel kb most

--- js.js
+++ js.js
@@ -129,3 +129,38 @@ if(document.calculator.LED.value[0] == ".")
    document.calculator.LED.value = "0" + document.calculator.LED.value;
 }
 
+function kik(){
+  var ki='<center><div class="btn-group"><button class="button" onclick="kezd()">A kezdetek: Az abakusz</button><button class="button" onclick="th()">A 17. század</button><button class="button" onclick="tk()">A 19. század</button><button class="button" onclick="hh()">1930–1970</button><button class="button" onclick="ot()">1950</button><button class="button" onclick="vissza()">X</button></div></center>'
+  document.getElementById("ide").innerHTML = ki;
+}
+
+function kezd(){
+  var ki='<center><div class="media border p-3 h-100"><h3>A kezdetek: Az abakusz</h3><p>Az első, a számítási feladatot megkönnyítő eszköz az abakusz volt. Fa kereten fémrudakon golyókat lehet húzni. Sokkal régebbi, mint az arab számrendszer. Régen elterjedt volt mindenhol, de ma már csak Kínában használják a kereskedők.</p><br><a href="https://onlineszamologep.hu/" target="_blank"><button class="forras">Forrás</button></a></div></center>'
+  document.getElementById("oda").innerHTML = ki;
+}
+
+function th(){
+  var ki='<center><div class="media border p-3 h-100"><h3>A 17. század</h3><p>1623-ban Wilhelm Schickard építette az első automatikus számológépet, aminek a számoló óra nevet adta. 22 évvel később, 1645-ben Blaise Pascal francia filozófus megalkotta a később Pascaline néven ismertté vált szerkezetet, amit 1799-ig használtak az adók kiszámításához. A német filozófus, Leibniz Calculus ratiocinatort.</p><br><a href="https://onlineszamologep.hu/" target="_blank"><button class="forras">Forrás</button></a></div></center>'
+  document.getElementById("oda").innerHTML = ki;
+}
+
+function tk(){
+  var ki='<center><div class="media border p-3 h-100"><h3>A 19. század</h3><p>Charles Babbage létrehozta azt a rendszert, ami alapján a mai programozható számítógépek működnek. De amit megépített, túl nehéz volt ahhoz, hogy működtetni lehetett volna.</p><br><a href="https://onlineszamologep.hu/" target="_blank"><button class="forras">Forrás</button></a></div></center>'
+  document.getElementById("oda").innerHTML = ki;
+}
+
+function hh(){
+  var ki='<center><div class="media border p-3 h-100"><h3>1930–1970</h3><p>Az 1930-as évektől az 1970-es évekig a mechanikus számológépek uralták a piacot. A legnagyobb gyártók közé tartozott a Friden, a Monroe és az SCM/Marchant. Ezeket a szerkezeteket motor hajtotta. Az összeadás és a kivonás az egyszerű összeadógépek mintáján működött, de a szorzás csak ismételt összeadás, az osztás pedig ismételt kivonás útján volt megvalósítható. A kézi meghajtású eszközöket, mint amilyen például az 1948-as fejlesztésű Curta, az 1970-es évek végéig használatban voltak.</p><br><a href="https://onlineszamologep.hu/"><button class="forras" target="_blank">Forrás</button></a></div></center>'
+  document.getElementById("oda").innerHTML = ki;
+}
+
+function ot(){
+  var ki='<center><div class="media border p-3 h-100"><h3>1950</h3><p>1954-ben az IBM bemutatott egy nagy, csak tranzisztorokkal dolgozó számológépet, és 1957-ben piacra dobták az első kereskedelmi példányt. (IBM 608) 1961 elején, megcsinálták az első, teljesen elektromosan működő számológépét, a Bell Punch/Sumlock Comptometer ANITA (A New Inspiration To Arithmetic) Mk.VII-etA gépben talált hibákat szeptemberre kijavították, és kijött az új, sokkal hatékonyabb változat, a Mark VIII. Ez volt a legjobb számológép egészen 1953-ig.</p><br><a href="https://onlineszamologep.hu/"><button class="forras" target="_blank">Forrás</button></a></div></center>'
+  document.getElementById("oda").innerHTML = ki;
+}
+
+function vissza(){
+  var ki='<center><button type="button" class="gombi" onclick="kik()">Számológépek története</button></center>'
+  document.getElementById("ide").innerHTML = ki;
+  document.getElementById("oda").innerHTML = "";
+}          
--- js2.js
+++
@@ -1,4 +0,0 @@
-function tortenet(){
-    var ki=' <div class="btn-group"><button class="button">Button</button><button class="button">Button</button><button class="button">Button</button><button class="button">Button</button></div>'
-    document.getElementById("ide").innerHTML=ki;
-  }
